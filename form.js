c6c9aa396e4d79968fbe45dfa8c1806a43766ffc
siddhesh-pawar <sidpawar2398@gmail.com>
2023-12-06 13:51:42 -0800

    User registration and login

--- form.js
+++ form.js
@@ -2,14 +2,14 @@ function validateForm() {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    var repassword = document.getElementById("repassword").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
 
-    if (name === "" || email === "" || password === "" || repassword === "") {
+    if (name === "" || email === "" || password === "" || confirmPassword === "") {
         alert("All fields are required");
         return false;
     }
 
-    if (password !== repassword) {
+    if (password !== confirmPassword) {
         alert("Passwords do not match");
         return false;
     }
